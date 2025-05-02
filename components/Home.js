39a5c601a501aa439760c8eaf70b3ec6360c9e06
antoine <antoine.boisard8@gmail.com>
2025-05-02 16:27:16 +0200

    add meta and h1 and h2

--- components/Home.js
+++ components/Home.js
@@ -1,13 +1,22 @@
 import styles from '../styles/Home.module.scss';
+import Image from 'next/image'
 
 function Home() {
   return (
     <div>
       <main className={styles.main}>
-        {/* <h1 className={styles.title}>
-          Paname Studio
-        </h1> */}
-        <img src="./img/paname-studio-logo-01.png" className={styles.panameStudioLogo} alt="Paname Studio" />
+        <h1 className={styles.title}>
+          <Image
+            src="/img/paname-studio-logo-01.png"
+            alt="Paname Studio - création digitale - Paris"
+            width={1023}
+            height={520}
+            className={styles.logo}
+          />
+        </h1>
+        <h2 className={styles.subTitle}>
+         Création digitale
+        </h2> 
       </main>
     </div>
   );
--- pages/_app.js
+++ pages/_app.js
@@ -5,11 +5,35 @@ function App({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <meta charSet="UTF-8" /> 
-        <meta name="viewport" content="width=device-width, initial-scale=1"></meta>
+        {/* Métadonnées globales */}
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="robots" content="index, follow" />
+
+        {/* SEO */}
         <title>Paname Studio – Créatif & Digital</title>
-        <meta name="description" content={'Paname Studio est un studio créatif à Paris spécialisé en identité visuelle, photo, vidéo et création web. Donnez vie à votre univers digital avec style.'} />
-        <link rel="preconnect" href="https://fonts.googleapis.com"></link>
+        <meta
+          name="description"
+          content="Paname Studio est un studio créatif à Paris spécialisé en création web, identité visuelle, photo et vidéo. Donnez vie à votre univers digital avec style."
+        />
+
+        {/* Open Graph (réseaux sociaux) */}
+        <meta property="og:title" content="Paname Studio – Créatif & Digital" />
+        <meta
+          property="og:description"
+          content="Studio créatif à Paris spécialisé en création de sites web, photographie, vidéo et identité visuelle."
+        />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://www.paname-studio.fr/" />
+        <meta property="og:image" content="https://www.paname-studio.fr/img/paname-studio-logo-01.png" />
+
+        {/* Google Fonts */}
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link
+        href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600&family=Share+Tech+Mono&display=swap"
+        rel="stylesheet"
+      />
       </Head>
       <Component {...pageProps} />
     </>
