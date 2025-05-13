32b692bb9536ab6a4f0fe4030a70b20ab45bc8db
antoine <antoine.boisard8@gmail.com>
2025-05-13 17:10:57 +0200

    add robots txt

---
+++ next-sitemap.config.js
@@ -0,0 +1,10 @@
+// next-sitemap.config.js
+module.exports = {
+    siteUrl: 'https://www.paname-studio.fr',
+    generateRobotsTxt: true,
+    sitemapSize: 7000,
+    changefreq: 'monthly',
+    priority: 0.7,
+    exclude: ['/404'],
+  };
+  
