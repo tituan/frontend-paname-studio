60763396b7a69b7e5376b5d672131556383c92a2
antoine <antoine.boisard8@gmail.com>
2025-05-13 17:16:19 +0200

    scriot gtm head

--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,3 @@
-// pages/_document.js
 import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
@@ -11,6 +10,17 @@ export default function Document() {
         <link
           href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600&family=Share+Tech+Mono&display=swap"
           rel="stylesheet"
+        />
+
+        {/* Google Tag Manager - HEAD */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-595GFJ57');`,
+          }}
         />
 
         {/* CookieConsent - Osano */}
@@ -32,6 +42,16 @@ export default function Document() {
         `}</style>
       </Head>
       <body>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-595GFJ57"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          ></iframe>
+        </noscript>
+
         {/* CookieConsent config */}
         <script
           dangerouslySetInnerHTML={{
@@ -56,20 +76,9 @@ export default function Document() {
           }}
         />
 
-        {/* Google Tag Manager (noscript) */}
-        <noscript>
-          <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-595GFJ57"
-            height="0"
-            width="0"
-            style={{ display: 'none', visibility: 'hidden' }}
-          ></iframe>
-        </noscript>
-
         <Main />
         <NextScript />
       </body>
     </Html>
   );
 }
-
