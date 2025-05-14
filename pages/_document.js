4f16a70f6f272a7796319c040323ec74c069f37b
antoine <antoine.boisard8@gmail.com>
2025-05-14 18:24:12 +0200

    change and add font barlow

--- pages/_document.js
+++ pages/_document.js
@@ -7,10 +7,8 @@ export default function Document() {
         {/* Google Fonts */}
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600&family=Share+Tech+Mono&display=swap"
-          rel="stylesheet"
-        />
+        <link href="https://fonts.googleapis.com/css2?family=Barlow:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet"/>
+
 
         {/* Google Tag Manager - HEAD */}
         <script
